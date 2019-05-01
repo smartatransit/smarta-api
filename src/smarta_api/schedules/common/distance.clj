9f549e328f661e8a6d4d6beacafe06ec75835904
Chris Smith <chris.smith@careerbuilder.com>
2019-04-30 23:31:35 -0400

    Added station-by-location endpoint (#15)

---
+++ src/smarta_api/schedules/common/distance.clj
@@ -0,0 +1,44 @@
+(ns smarta-api.schedules.common.distance
+  (:require [geo.spatial :as spatial]
+            [geo.geohash :as ghash]
+            [smarta-api.schedules.static.static-schedule :as stat]
+            [clojure.data.csv :as csv]
+            [clojure.java.io :as io]))
+
+(def station-file-location "data/location/stations.csv")
+
+(def meter-to-foot 3.28084)
+
+(def mile 5280)
+
+(defn load-stations []
+  (with-open [reader (io/reader station-file-location)]
+    (stat/rotate-matrix
+     (doall
+      (csv/read-csv reader)))))
+
+(defn get-stations []
+  (reduce
+   (fn [acc item]
+     (conj acc {:station-name (get item 0)
+                :location (get item 1)})) [] (load-stations)))
+
+(defn lat-long-to-geohash [lat long])
+
+(defn geohash-to-lat-long [geohash]
+  (let [center (ghash/geohash-center (ghash/geohash geohash))]
+    [(.getLatitude center) (.getLongitude center)]))
+
+(defn get-crow-distance [departure destination]
+  (/ (spatial/distance departure destination) 1000))
+
+(defn get-stations-by-distance [lat long]
+  (let [stations (get-stations)
+        current-address (spatial/spatial4j-point lat long)]
+    (sort-by :distance
+             (reduce
+              (fn [acc item]
+                (let [[station-lat station-long] (geohash-to-lat-long (:location item))
+                      station-point (spatial/spatial4j-point station-lat station-long)
+                      distance (get-crow-distance current-address station-point)]
+                  (conj acc (assoc item :distance distance)))) [] stations))))
